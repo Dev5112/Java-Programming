c7d5cead5949fbfa94386686eb20061254d85793
Dev5112 <debanjans231@gmail.com>
2024-11-10 20:33:44 +0530

    Add files via upload

---
+++ Sorting Algorithm/Q2.java
@@ -0,0 +1,22 @@
+package dev7;
+public class Q2 {
+	    public static void main(String[] args) {
+	        int[] arr = {34, 25, 12, 22, 90};
+	        int n = arr.length;
+	        insertionSort(arr);
+	        System.out.println("Sorted array:");
+	        printArray(arr);}
+	    public static void insertionSort(int[] arr) {
+	        int n = arr.length;
+	        for (int i = 1; i < n; i++) {
+	            int key = arr[i];
+	            int j = i - 1;
+	            while (j >= 0 && arr[j] > key) {
+	                arr[j + 1] = arr[j];
+	                j = j - 1;}
+	            arr[j + 1] = key;}}
+	    public static void printArray(int[] arr) {
+	        for (int i = 0; i < arr.length; i++) {
+	            System.out.print(arr[i] + " ");}}}
+	        
+	 
---
+++ Sorting Algorithm/Q3.java
@@ -0,0 +1,22 @@
+package dev7;
+public class Q3 {
+	    public static void main(String[] args) {
+	        int[] arr = {25, 12, 22, 11};
+	        int n = arr.length;
+	        selectionSort(arr);
+	        System.out.println("Sorted array:");
+	        printArray(arr);}
+	    public static void selectionSort(int[] arr) {
+	        int n = arr.length;
+	        for (int i = 0; i < n - 1; i++) {
+	            int minIndex = i;
+	            for (int j = i + 1; j < n; j++) {
+	                if (arr[j] < arr[minIndex]) {
+	                    minIndex = j;}}
+	            int temp = arr[minIndex];
+	            arr[minIndex] = arr[i];
+	            arr[i] = temp;}}
+	    public static void printArray(int[] arr) {
+	        for (int i = 0; i < arr.length; i++) {
+	            System.out.print(arr[i] + " ");}}}
+	        
