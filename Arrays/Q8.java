cb7162d152f6985a4a72e6910c048741b4e513c8
Dev5112 <debanjans231@gmail.com>
2024-11-23 23:22:15 +0530

    Add files via upload

---
+++ Arrays/Q8.java
@@ -0,0 +1,19 @@
+package dev8;
+public class m5 {
+	    public static void main(String[] args) {
+	        int[] numbers = {1, 3, 2, 3, 4, 3, 2, 1}; 
+	        int maxElement = findMaxOccurrence(numbers);
+	        System.out.println("Element with maximum occurrences: " + maxElement);}
+	    public static int findMaxOccurrence(int[] arr) {
+	        int maxCount = 0; 
+	        int maxElement = arr[0]; 
+	        for (int i = 0; i < arr.length; i++) {
+	            int count = 0;
+	            for (int j = 0; j < arr.length; j++) {
+	                if (arr[j] == arr[i]) {
+	                    count++;}}
+	            if (count > maxCount) {
+	                maxCount = count;
+	                maxElement = arr[i];}}
+	        return maxElement;}}
+
