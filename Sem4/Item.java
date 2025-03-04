4e43a554acc66e6cb34b1e0cc6a7d096c6b7eeb6
Dev5112 <debanjans231@gmail.com>
2025-03-04 17:49:38 +0530

    Update Item.java

--- Sem4/Item.java
+++ Sem4/Item.java
@@ -1,3 +1,10 @@
+/*Given n items with their weights and profits and a knapsack of capacity W, write a JAVA
+program to select the subset of items into the knapsack to maximize the profit of the selection
+and not exceeding the capacity of the knapsack. We can select a fraction of an item in order to
+maximize the total profit of the selected items.*/
+
+Item.java
+
 package prog1;
 import java.util.*;
 public class Item {
