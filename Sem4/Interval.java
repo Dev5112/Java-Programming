05ae162204155ad30c425b3bf49e89bc79941e13
Dev5112 <debanjans231@gmail.com>
2025-03-04 17:38:43 +0530

    Update Interval.java

--- Sem4/Interval.java
+++ Sem4/Interval.java
@@ -2,6 +2,8 @@
 maximal set of mutually compatible intervals those can be processed under a single available
 resource using Greedy approach.*/
 
+Interval.java
+	
 package prog1;
 public class Interval {
 	    int start;
@@ -14,5 +16,44 @@ public class Interval {
 	        return "[" + start + ", " + finish + "]";
 	    }
 	}
+
+MaxCompatibleIntervals.java
 	
+package prog1;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class MaxCompatibleIntervals {
+    public static List<Interval> findMaxCompatibleIntervals(Interval[] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a.finish, b.finish));
+        List<Interval> result = new ArrayList<>();
+        if (intervals.length == 0) {
+            return result;
+        }
+        result.add(intervals[0]);
+        int lastFinish = intervals[0].finish;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start >= lastFinish) {
+                result.add(intervals[i]);
+                lastFinish = intervals[i].finish;}}
+        return result;}
+    public static void main(String[] args) {
+        
+        Interval[] intervals = {
+            new Interval(1, 4),
+            new Interval(2, 5),
+            new Interval(7, 9),
+            new Interval(6, 8),
+            new Interval(4, 7)
+        };
+        System.out.println("Input intervals:");
+        for (Interval interval : intervals) {
+            System.out.print(interval + " ");
+        }
+        System.out.println("\n");
+        List<Interval> result = findMaxCompatibleIntervals(intervals);
+        System.out.println("Maximum compatible intervals:");
+        for (Interval interval : result) {
+            System.out.print(interval + " ");
+	}}}	
 
