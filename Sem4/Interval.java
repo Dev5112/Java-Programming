49f64698c1e4d37c2d175406a7962edbcc8e9109
Dev5112 <debanjans231@gmail.com>
2025-03-04 17:36:56 +0530

    Update Interval.java

--- Sem4/Interval.java
+++ Sem4/Interval.java
@@ -1,3 +1,7 @@
+/*Given a set of intervals with starting and finishing times, write a JAVA program to find the
+maximal set of mutually compatible intervals those can be processed under a single available
+resource using Greedy approach.*/
+
 package prog1;
 public class Interval {
 	    int start;
